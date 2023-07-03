40df02438c26e99577ebc82617adb79188e64c14
khaled <khaedmo@gmail.com>
2023-07-03 16:28:23 +0300

    adding refresh indicators

--- lib/App/modules/article/page.dart
+++ lib/App/modules/article/page.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
-
 import '../../../core/utils/helperFunctions.dart';
-import '../../../core/values/apiEndPoints.dart';
-import '../../../core/values/colors.dart';
 import '../../data/models/articleModel.dart';
-import '../../data/models/donationAppealModel.dart';
 import '../../widgets/fistPageErrorIndicator.dart';
 import 'controller.dart';
 
@@ -30,51 +26,56 @@ class ArticleScreen extends GetView<ArticleScreenController> {
               color: Colors.black),
         ),
       ),
-      body: PagedListView<int, ArticleModel>.separated(
-        pagingController: controller.pagingController,
-        physics: const BouncingScrollPhysics(),
-        builderDelegate: PagedChildBuilderDelegate<ArticleModel>(
-          firstPageErrorIndicatorBuilder: (context) {
-            return Padding(
-              padding: EdgeInsets.only(
-                  top: getMediaQueryHeight(context: context, value: 250)),
-              child: firstPageErrorIndicator(controller, () {
-                controller.pagingController.retryLastFailedRequest();
-              }),
-            );
-          },
-          newPageErrorIndicatorBuilder: (context) {
-            return InkWell(
-              onTap: () {
-                controller.pagingController.retryLastFailedRequest();
-              },
-              child: const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Text("حدث خطأ ما.اضعط للمحاولة مجددا"),
-                    Icon(Icons.refresh)
-                  ],
+      body: RefreshIndicator(
+        onRefresh: () {
+          return Future.sync(() => controller.pagingController.refresh());
+        },
+        child: PagedListView<int, ArticleModel>.separated(
+          pagingController: controller.pagingController,
+          physics: const BouncingScrollPhysics(),
+          builderDelegate: PagedChildBuilderDelegate<ArticleModel>(
+            firstPageErrorIndicatorBuilder: (context) {
+              return Padding(
+                padding: EdgeInsets.only(
+                    top: getMediaQueryHeight(context: context, value: 250)),
+                child: firstPageErrorIndicator(controller, () {
+                  controller.pagingController.retryLastFailedRequest();
+                }),
+              );
+            },
+            newPageErrorIndicatorBuilder: (context) {
+              return InkWell(
+                onTap: () {
+                  controller.pagingController.retryLastFailedRequest();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text("حدث خطأ ما.اضعط للمحاولة مجددا"),
+                      Icon(Icons.refresh)
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-          animateTransitions: true,
-          itemBuilder: (context, item, index) {
-            print(item);
-            return CustomCard(
-              imageUrl: '${item.imageUrl}',
-              title: item.title!,
-              supTitle: item.description!,
-              articlUrl: item.url!,
+              );
+            },
+            animateTransitions: true,
+            itemBuilder: (context, item, index) {
+              print(item);
+              return CustomCard(
+                imageUrl: '${item.imageUrl}',
+                title: item.title!,
+                supTitle: item.description!,
+                articlUrl: item.url!,
+              );
+            },
+          ),
+          separatorBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: getMediaQueryHeight(context: context, value: 16),
             );
           },
         ),
-        separatorBuilder: (BuildContext context, int index) {
-          return SizedBox(
-            height: getMediaQueryHeight(context: context, value: 16),
-          );
-        },
       ),
     );
   }
--- lib/App/modules/home/homePageContent.dart
+++ lib/App/modules/home/homePageContent.dart
@@ -31,157 +31,163 @@ class HomeScreenContent extends GetView<HomeScreenController> {
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          GetBuilder(builder: (HomeScreenController controller) {
-            return controller.campaignsIsLoading
-                ? const CustomLoadingWidget()
-                : controller.campaigns!.isEmpty
-                    ? SingleErrorTextWidget(
-                        errorMessage: 'لا يوجد حملات في الوقت الحالي !',
-                        height: 200,
-                      )
-                    : CarouselSlider(
-                        items: controller.campaigns!
-                            .map((e) => Image.network(
-                                  e.imageUrl!,
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
-                                ))
-                            .toList(),
-                        options: CarouselOptions(
-                          onPageChanged: (index, reason) {
-                            controller.changeCurrentBannerIndex(index);
-                          },
-                          viewportFraction: 1,
-                          height:
-                              getMediaQueryHeight(context: context, value: 200),
-                        ),
-                      );
-          }),
-          SizedBox(
-            height: getMediaQueryHeight(context: context, value: 8),
-          ),
-          GetBuilder(
-            builder: (HomeScreenController controller) {
+      RefreshIndicator(
+        onRefresh: () async {
+          await controller.fetchCampaigns();
+          return Future.sync(() => controller.pagingController.refresh());
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GetBuilder(builder: (HomeScreenController controller) {
               return controller.campaignsIsLoading
-                  ? const SizedBox()
-                  : controller.errorMessage!.isNotEmpty
-                      ? const SizedBox()
-                      : controller.campaigns!.isEmpty
-                          ? const SizedBox()
-                          : Center(
-                              child: SmoothPageIndicator(
-                              controller: PageController(
-                                  initialPage: controller.currentBannerIndex),
-                              count: controller.campaigns!.length,
-                              effect: const ExpandingDotsEffect(
-                                  activeDotColor: Colors.red,
-                                  dotHeight: 10,
-                                  dotWidth: 10),
-                            ));
-            },
-          ),
-          SizedBox(
-            height: getMediaQueryHeight(context: context, value: 24),
-          ),
-          CustomButton(
-            onPressed: () {
-              bool? isUserVerified = Get.find<AppSharedPref>()
-                  .getBoolValue(key: isEmailVerifiedKey);
-              if (controller.isLoggedIn != null &&
-                  controller.isLoggedIn == true &&
-                  isUserVerified != null &&
-                  isUserVerified == true) {
-                Get.toNamed(Routes.DONATION_FORM);
-              } else if (controller.isLoggedIn != null &&
-                  controller.isLoggedIn == true &&
-                  isUserVerified != null &&
-                  isUserVerified == false) {
-                customDialog(
-                    context: context,
-                    title: "العملية تحتاج الى توثيق البريد الالكتروني",
-                    controller: controller,
-                    btnOkText: "توثيق البريد الالكتروني",
-                    onDismissCallback: () {
-                      controller.blurScreenFun(false);
-                    },
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {
-                      Get.back();
-                      Get.toNamed("/sendVerificationCode");
-                    }).show();
-              } else {
-                controller.blurScreenFun(true);
-                customDialog(
-                    context: context,
-                    controller: controller,
-                    onDismissCallback: () {
-                      controller.blurScreenFun(false);
-                    },
-                    btnCancelOnPress: () {},
-                    btnOkOnPress: () {
-                      Get.toNamed(Routes.LOGIN);
-                    }).show();
-              }
-            },
-            text: 'أنشئ طلب تبرع',
-          ),
-          SizedBox(
-            height: getMediaQueryHeight(context: context, value: 32),
-          ),
-          Text(
-            "مناشدات للتبرع بالدم",
-            style: TextStyle(fontSize: 20.spMin, fontWeight: FontWeight.w700),
-          ),
-          SizedBox(
-            height: getMediaQueryHeight(context: context, value: 24),
-          ),
-          Expanded(
-            child: RefreshIndicator(
-              onRefresh: () {
-                return Future.sync(() => controller.pagingController.refresh());
-              },
-              child: PagedListView<int, DonationAppealModel>.separated(
-                pagingController: controller.pagingController,
-                builderDelegate: PagedChildBuilderDelegate<DonationAppealModel>(
-                    firstPageErrorIndicatorBuilder: (context) {
-                      return firstPageErrorIndicator(controller, () {
-                        controller.pagingController.retryLastFailedRequest();
-                      });
-                    },
-                    newPageErrorIndicatorBuilder: (context) {
-                      return InkWell(
-                        onTap: () {
-                          controller.pagingController.retryLastFailedRequest();
-                        },
-                        child: const Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              Text("حدث خطأ ما.اضعط للمحاولة مجددا"),
-                              Icon(Icons.refresh)
-                            ],
+                  ? const CustomLoadingWidget()
+                  : controller.campaigns!.isEmpty
+                      ? SingleErrorTextWidget(
+                          errorMessage: 'لا يوجد حملات في الوقت الحالي !',
+                          height: 200,
+                        )
+                      : CarouselSlider(
+                          items: controller.campaigns!
+                              .map((e) => Image.network(
+                                    e.imageUrl!,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
+                                  ))
+                              .toList(),
+                          options: CarouselOptions(
+                            onPageChanged: (index, reason) {
+                              controller.changeCurrentBannerIndex(index);
+                            },
+                            viewportFraction: 1,
+                            height:
+                                getMediaQueryHeight(context: context, value: 200),
                           ),
-                        ),
-                      );
-                    },
-                    animateTransitions: true,
-                    itemBuilder: (context, item, index) {
-                      return CustomAppealCard(
-                        donationAppealModel: item,
-                      );
-                    }),
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    height: getMediaQueryHeight(context: context, value: 16),
-                  );
-                },
-              ),
+                        );
+            }),
+            SizedBox(
+              height: getMediaQueryHeight(context: context, value: 8),
             ),
-          )
-        ],
+            GetBuilder(
+              builder: (HomeScreenController controller) {
+                return controller.campaignsIsLoading
+                    ? const SizedBox()
+                    : controller.errorMessage!.isNotEmpty
+                        ? const SizedBox()
+                        : controller.campaigns!.isEmpty
+                            ? const SizedBox()
+                            : Center(
+                                child: SmoothPageIndicator(
+                                controller: PageController(
+                                    initialPage: controller.currentBannerIndex),
+                                count: controller.campaigns!.length,
+                                effect: const ExpandingDotsEffect(
+                                    activeDotColor: Colors.red,
+                                    dotHeight: 10,
+                                    dotWidth: 10),
+                              ));
+              },
+            ),
+            SizedBox(
+              height: getMediaQueryHeight(context: context, value: 24),
+            ),
+            CustomButton(
+              onPressed: () {
+                bool? isUserVerified = Get.find<AppSharedPref>()
+                    .getBoolValue(key: isEmailVerifiedKey);
+                if (controller.isLoggedIn != null &&
+                    controller.isLoggedIn == true &&
+                    isUserVerified != null &&
+                    isUserVerified == true) {
+                  Get.toNamed(Routes.DONATION_FORM);
+                } else if (controller.isLoggedIn != null &&
+                    controller.isLoggedIn == true &&
+                    isUserVerified != null &&
+                    isUserVerified == false) {
+                  customDialog(
+                      context: context,
+                      title: "العملية تحتاج الى توثيق البريد الالكتروني",
+                      controller: controller,
+                      btnOkText: "توثيق البريد الالكتروني",
+                      onDismissCallback: () {
+                        controller.blurScreenFun(false);
+                      },
+                      btnCancelOnPress: () {},
+                      btnOkOnPress: () {
+                        Get.back();
+                        Get.toNamed("/sendVerificationCode");
+                      }).show();
+                } else {
+                  controller.blurScreenFun(true);
+                  customDialog(
+                      context: context,
+                      controller: controller,
+                      onDismissCallback: () {
+                        controller.blurScreenFun(false);
+                      },
+                      btnCancelOnPress: () {},
+                      btnOkOnPress: () {
+                        Get.toNamed(Routes.LOGIN);
+                      }).show();
+                }
+              },
+              text: 'أنشئ طلب تبرع',
+            ),
+            SizedBox(
+              height: getMediaQueryHeight(context: context, value: 32),
+            ),
+            Text(
+              "مناشدات للتبرع بالدم",
+              style: TextStyle(fontSize: 20.spMin, fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: getMediaQueryHeight(context: context, value: 24),
+            ),
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: () {
+                  return Future.sync(() => controller.pagingController.refresh());
+                },
+                child: PagedListView<int, DonationAppealModel>.separated(
+                  pagingController: controller.pagingController,
+                  builderDelegate: PagedChildBuilderDelegate<DonationAppealModel>(
+                      firstPageErrorIndicatorBuilder: (context) {
+                        return firstPageErrorIndicator(controller, () {
+                          controller.pagingController.retryLastFailedRequest();
+                        });
+                      },
+                      newPageErrorIndicatorBuilder: (context) {
+                        return InkWell(
+                          onTap: () {
+                            controller.pagingController.retryLastFailedRequest();
+                          },
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Column(
+                              children: [
+                                Text("حدث خطأ ما.اضعط للمحاولة مجددا"),
+                                Icon(Icons.refresh)
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      animateTransitions: true,
+                      itemBuilder: (context, item, index) {
+                        return CustomAppealCard(
+                          donationAppealModel: item,
+                        );
+                      }),
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: getMediaQueryHeight(context: context, value: 16),
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       GetBuilder<HomeScreenController>(builder: (controller) {
         if (controller.blurScreen) {
